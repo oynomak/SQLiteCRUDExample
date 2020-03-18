ee271a8d67f7d597fe2800023f4da85f02c4adfd
oynomak <oynomak@gmail.com>
2020-03-18 20:59:09 +0000

    Added permission to Manifest to send SMS. Also started the SendSMS functionality

--- app/src/main/java/edu/oynomak/sqlitecrudexample/SendSMSActivity.java
+++ app/src/main/java/edu/oynomak/sqlitecrudexample/SendSMSActivity.java
@@ -14,8 +14,7 @@ import android.widget.Toast;
 public class SendSMSActivity extends AppCompatActivity {
 
     Button buttonSend;
-    EditText textPhoneNo;
-    EditText textSMS;
+    EditText textPhoneNo, textSMS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +31,26 @@ public class SendSMSActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String phoneNo = textPhoneNo.getText().toString();
-                String sms = textSMS.getText().toString();
+                String smsBody = textSMS.getText().toString();
 
                 try {
                     //Getting intent and PendingIntent instance
-                    //Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    //Intent intent = new Intent(getApplicationContext(),SendSMSActivity.class);
                     //PendingIntent pi = PendingIntent.getActivity(SendSMSActivity.this, 0, intent,0);
+                    Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+                    sendIntent.putExtra("sms_body", smsBody);
+                    sendIntent.putExtra("address", phoneNo);
+                    sendIntent.setType("vnd.android-dir/mms-sms");
+                    startActivity(sendIntent);
 
                     // Check self permission
                     //checkSelfPermission();
 
 
                     //Get the SmsManager instance and call the sendTextMessage method to send message
-                    SmsManager smsManager = SmsManager.getDefault();
+                    //SmsManager smsManager = SmsManager.getDefault();
 
-                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    //smsManager.sendTextMessage(phoneNo, null, sms, null, null);
                     //smsManager.sendTextMessage(phoneNo, null, sms, pi,null);
                     Toast.makeText(getApplicationContext(), "SMS Sent!",
                             Toast.LENGTH_LONG).show();
