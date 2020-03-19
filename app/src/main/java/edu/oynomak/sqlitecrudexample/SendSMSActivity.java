4fe1bdb5eddd27174875e762b8a1e05cf5737b78
oynomak <oynomak@gmail.com>
2020-03-19 13:41:07 +0000

    Enhancing encapsulation and reusability of code...

--- app/src/main/java/edu/oynomak/sqlitecrudexample/SendSMSActivity.java
+++ app/src/main/java/edu/oynomak/sqlitecrudexample/SendSMSActivity.java
@@ -35,31 +35,9 @@ public class SendSMSActivity extends AppCompatActivity {
                 String smsBody = textSMS.getText().toString();
 
                 try {
-                    // --- Getting intent and PendingIntent instance ---
-                    /*Intent intent = new Intent(getApplicationContext(),SendSMSActivity.class);
-                    PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, intent,0);*/
+                    // Sending sms...
+                    sendSMS(phoneNo, smsBody);
 
-                    // --- Using Intent + startActivity() ---
-                    //*
-                    Intent sendIntent = new Intent(getApplicationContext(),SendSMSActivity.class);
-                    sendIntent.putExtra("sms_body", smsBody);
-                    //sendIntent.setData(Uri.parse("smsto:"));
-                    sendIntent.putExtra("address", phoneNo);
-                    sendIntent.setType("vnd.android-dir/mms-sms");
-                    startActivity(sendIntent);
-                    //*/
-
-                    // Check self permission
-                    //checkSelfPermission();
-
-
-                    //Get the SmsManager instance and call the sendTextMessage method to send message
-                    //SmsManager smsManager = SmsManager.getDefault();
-
-                    //smsManager.sendTextMessage(phoneNo, null, sms, null, null);
-                    //smsManager.sendTextMessage(phoneNo, null, smsBody, pi,null);
-                    Toast.makeText(getApplicationContext(), "SMS Sent to: "+ phoneNo +" was: \n <<"+smsBody+">>",
-                            Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
                     Toast.makeText(getApplicationContext(),
                             "SMS failed, please try again later!",
@@ -69,5 +47,33 @@ public class SendSMSActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void sendSMS(String phoneNo, String smsBody) {
+
+        // --- Getting intent and PendingIntent instance ---
+        /*Intent intent = new Intent(getApplicationContext(),SendSMSActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, intent,0);*/
+        // --- Using Intent + startActivity() ---
+        //*
+        Intent sendIntent = new Intent(getApplicationContext(), SendSMSActivity.class);
+        sendIntent.putExtra("sms_body", smsBody);
+        //sendIntent.setData(Uri.parse("smsto:"));
+        sendIntent.putExtra("address", phoneNo);
+        sendIntent.setType("vnd.android-dir/mms-sms");
+        startActivity(sendIntent);
+        //*/
+
+        // Check self permission
+        //checkSelfPermission();
+
+
+        //Get the SmsManager instance and call the sendTextMessage method to send message
+        //SmsManager smsManager = SmsManager.getDefault();
+
+        //smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+        //smsManager.sendTextMessage(phoneNo, null, smsBody, pi,null);
+        Toast.makeText(getApplicationContext(), "SMS Sent to: "+ phoneNo +" was: \n <<"+smsBody+">>",
+                Toast.LENGTH_LONG).show();
     }
 }
